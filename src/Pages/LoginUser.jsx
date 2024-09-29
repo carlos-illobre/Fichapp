9c3d379406dae0d9b8c089439b84d12af491e713
kuikuiks1 <cazcoagapuyo@uade.edu.ar>
2024-09-28 23:51:29 -0300

    Se elimino el campo de Ingresar Nombre

--- src/Pages/LoginUser.jsx
+++ src/Pages/LoginUser.jsx
@@ -3,12 +3,10 @@ import "./CSS/LoginSignup.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setUser } from "../ReduxToolkit/userSlice";
-import { auth, googleProvider } from "../firebase"; 
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
 const LoginUser = () => {
   const dispatch = useDispatch();
-  const [registro, setRegistro] = useState({role:"ADMIN",name:"", email: "", password: "", isLogged: false });
+  const [registro, setRegistro] = useState({ email: "", password: "", isLogged: false });
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
@@ -16,7 +14,7 @@ const LoginUser = () => {
     setRegistro({ ...registro, [target.name]: target.value, isLogged: true });
   };
 
-  const handleContinuarClick = async () => {
+  const handleContinuarClick = () => {
     const { email, password } = registro;
 
     if (!email || !password) {
@@ -24,32 +22,9 @@ const LoginUser = () => {
       return;
     }
 
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-      console.log(user.email);
-      dispatch(setUser({ ...registro, isLogged: true })); // actualizar el estado del usuario en Redux
-      navigate("/");
-      setErrorMessage("");
-    } catch (error) {
-      setErrorMessage("Error al iniciar sesión. Verifica tus credenciales.");
-    }
-  };
-
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, googleProvider);
-      const user = result.user;
-      dispatch(setUser({
-        name: user.displayName,
-        email: user.email,
-        role: "ADMIN", // Aquí puedes ajustar según tu lógica de roles
-        isLogged: true
-      }));
-      navigate("/");
-    } catch (error) {
-      setErrorMessage("Error al iniciar sesión con Google.");
-    }
+    dispatch(setUser(registro)); // actualizar el estado del usuario en Redux
+    navigate("/");
+    setErrorMessage("");
   };
 
   useEffect(() => {
@@ -82,7 +57,6 @@ const LoginUser = () => {
           </p>
         )}
         <button onClick={handleContinuarClick}>Iniciar Sesión</button>
-        <button onClick={handleGoogleLogin}>Iniciar Sesión con Google</button>
         <p className="loginsignup-login">
           No tienes una cuenta?{" "}
           <span onClick={() => navigate("/loginSignUp")}>Registrate</span>
