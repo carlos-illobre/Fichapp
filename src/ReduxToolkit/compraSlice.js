53fbf2a6ef21418616fec0e6f1148e877c492a53
Elisheba Tawil <sabuttawil@gmail.com>
2024-11-19 02:35:34 -0300

    manda los mails al comprador y al vendedor

--- src/ReduxToolkit/compraSlice.js
+++ src/ReduxToolkit/compraSlice.js
@@ -1,20 +1,36 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { db } from "../firebase"; // Importa la instancia de Firestore
-import {
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  doc,
-  deleteDoc,
-  getDoc,
-  where,
-  query,
-  setDoc,
-  get,
-} from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+import emailjs from "emailjs-com";
 
-// Acción asíncrona para agregar la compra a la colección "compras"
+const sendEmailComprador = async (templateParams) => {
+  try {
+    const result = await emailjs.send(
+      "service_q3xt5r8", // Reemplaza con tu Service ID
+      "template_k1gwgf3", // Reemplaza con tu Template ID
+      templateParams, // Parámetros dinámicos
+      "FrWU1KMdo5af0RSgr" // Reemplaza con tu Public Key
+    );
+    console.log("Correo comprador enviado:", result.text);
+  } catch (error) {
+    console.error("Error al enviar el correo comprador:", error);
+  }
+};
+
+const sendEmailVendedor = async (templateParams) => {
+  try {
+    const result = await emailjs.send(
+      "service_q3xt5r8", //Service ID
+      "template_oz21rc9", //  Template ID
+      templateParams, // Parámetros dinámicos
+      "FrWU1KMdo5af0RSgr" //  Public Key
+    );
+    console.log("Correo vendedor enviado :", result.text);
+  } catch (error) {
+    console.error("Error al enviar el correo vendedor:", error);
+  }
+};
+
 export const compraCarrito = createAsyncThunk(
   "compra/compraCarrito",
   async (
@@ -42,6 +58,48 @@ export const compraCarrito = createAsyncThunk(
       // Agregar a Firebase
       const docRef = await addDoc(collection(db, "compras"), nuevaCompra);
 
+      const emailParams = {
+        to_name: nameComprador,
+        to_email: emailComprador,
+        tipo_compra: "por carrito",
+        message: `Gracias por su compra. El total es $${totalAmount}. Aquí está el detalle de su compra:\n\n${items
+          .map((item) => {
+            const juego = item.juego || "Juego desconocido"; // Valor por defecto si juego es null
+            const nombre = item.nombre || "Producto desconocido"; // Manejo de nombre vacío
+            const cantidad = item.cantidad || 0;
+            const precio = item.precio || 0;
+            const subtotal = cantidad * precio;
+
+            return `Del juego ${juego} - ${nombre}: ${cantidad} x $${precio} (Subtotal: $${subtotal})`;
+          })
+          .join("\n")}\n\n¡Gracias por confiar en nosotros!`,
+      };
+
+      // Llama a la función de envío de correo
+      sendEmailComprador(emailParams);
+
+      const discount = 0.1; // El 10% en formato decimal
+      const totalAfterDiscount = totalAmount - totalAmount * discount;
+      const emailParamsVendedor = {
+        to_email: items[0].owner,
+        tipo_venta: "por carrito",
+        message: `El total de tu venta es de $${totalAfterDiscount}. Una vez hecha la entrega del producto se te va a estar acreditando el pago en tu cuenta
+        Aquí está el detalle de su venta:\n\n${items
+          .map((item) => {
+            const juego = item.juego || "Juego desconocido"; // Valor por defecto si juego es null
+            const nombre = item.nombre || "Producto desconocido"; // Manejo de nombre vacío
+            const cantidad = item.cantidad || 0;
+            const precio = item.precio || 0;
+            const subtotal = cantidad * precio;
+
+            return `Del juego ${juego} - ${nombre}: ${cantidad} x $${precio} (Subtotal: $${subtotal})`;
+          })
+          .join("\n")}\n\n¡Gracias por confiar en nosotros!`,
+      };
+
+      // Llama a la función de envío de correo
+      sendEmailVendedor(emailParamsVendedor);
+
       // Retornar el objeto de la compra con su ID
       return { id: docRef.id, ...nuevaCompra };
     } catch (error) {
@@ -64,7 +122,6 @@ export const compra3D = createAsyncThunk(
     thunkAPI
   ) => {
     try {
-      // Crear el objeto de la compra
       const nuevaCompra = {
         totalAmount,
         promoCodeApplied,
@@ -72,8 +129,6 @@ export const compra3D = createAsyncThunk(
         nameComprador,
         owner,
         items: items.map((item) => ({
-          // nombre: user.name || "",
-          // email: user.email || "",
           telefono: item.telefono,
           direccion: item.direccion,
           descripcion: item.descripcion,
@@ -83,6 +138,43 @@ export const compra3D = createAsyncThunk(
         createdAt: new Date().toISOString(),
         tipoCompra: "3D",
       };
+      const emailParams = {
+        to_name: nameComprador,
+        to_email: emailComprador,
+        tipo_compra: "servicio 3D",
+        message: `Gracias por su compra. El total es $${totalAmount}. Aquí está el detalle de su compra:\n\n${items
+          .map((item) => {
+            const descripcion = item.descripcion;
+            const observaciones = item.observaciones;
+
+            return `su pedido de impresion 3D fue ${descripcion} con observaciones ${observaciones}`;
+          })
+          .join("\n")}\n\n¡Gracias por confiar en nosotros!`,
+      };
+
+      // Llama a la función de envío de correo
+      sendEmailComprador(emailParams);
+
+      const discount = 0.15; // El 15% en formato decimal
+      const totalAfterDiscount = totalAmount - totalAmount * discount;
+      const emailParamsVendedor = {
+        to_email: owner,
+        tipo_venta: "por servicio impresión 3D",
+        message: `El total de tu venta es de $${totalAfterDiscount}. Una vez hecha la entrega del producto se te va a estar acreditando el pago en tu cuenta
+        Aquí está el detalle de su venta:\n\n${items
+          .map((item) => {
+            const descripcion = item.descripcion || "Descripción no disponible";
+            const observaciones = item.observaciones || "No hay observaciones.";
+            const archivoUrl = item.archivo;
+
+            return `El pediddo es con esta descripcion: ${descripcion} y el cliente hizo estas observaciones ${observaciones}
+            y la imagen del producto que se mando es la siguiente ${archivoUrl}`;
+          })
+          .join("\n")}\n\n¡Gracias por confiar en nosotros!`,
+      };
+
+      // Llama a la función de envío de correo
+      sendEmailVendedor(emailParamsVendedor);
 
       // Agregar a Firebase
       const docRef = await addDoc(collection(db, "compras"), nuevaCompra);
@@ -130,6 +222,46 @@ export const compraEmpresa = createAsyncThunk(
 
       const docRef = await addDoc(collection(db, "compras"), nuevaCompra);
 
+      const emailParams = {
+        to_name: nameComprador,
+        to_email: emailComprador,
+        tipo_compra: "pedido a Empresa",
+        message: `Gracias por su compra. El total es $${totalAmount}. Aquí está el detalle de su compra:\n\n${items
+          .map((item) => {
+            const descripcion = item.descripcion;
+            const observaciones = item.observaciones;
+
+            return `su pedido de ficha fue sobre el juego ${juego} con descripcion de la ficha 
+            ${descripcion} con observaciones ${observaciones}`;
+          })
+          .join("\n")}\n\n¡Gracias por confiar en nosotros!`,
+      };
+
+      // Llama a la función de envío de correo
+      sendEmailComprador(emailParams);
+
+      const discount = 0.15; // El 15% en formato decimal
+      const totalAfterDiscount = totalAmount - totalAmount * discount;
+      const emailParamsVendedor = {
+        to_email: owner,
+        tipo_venta: "por publicación de empresa",
+        message: `El total de tu venta es de $${totalAfterDiscount}. Una vez hecha la entrega del producto se te va a estar acreditando el pago en tu cuenta
+        Aquí está el detalle de su venta:\n\n${items
+          .map((item) => {
+            const descripcion = item.descripcion || "Descripción no disponible";
+            const observaciones = item.observaciones || "No hay observaciones.";
+            const archivoUrl = item.archivo;
+
+            return `El pedido es sobre el juego ${juego}. 
+            El pediddo es con esta descripcion: ${descripcion} y el cliente hizo estas observaciones ${observaciones}
+            y la imagen del producto que se mando es la siguiente ${archivoUrl}`;
+          })
+          .join("\n")}\n\n¡Gracias por confiar en nosotros!`,
+      };
+
+      // Llama a la función de envío de correo
+      sendEmailVendedor(emailParamsVendedor);
+
       return { id: docRef.id, ...nuevaCompra };
     } catch (error) {
       console.log("error" + error);
@@ -150,9 +282,11 @@ const compraSlice = createSlice({
       .addCase(compraCarrito.pending, (state) => {
         state.status = "loading";
       })
-      .addCase(compraCarrito.fulfilled, (state) => {
+      .addCase(compraCarrito.fulfilled, (state, action) => {
         state.status = "succeeded";
         state.error = null;
+        console.log("Items recibidos:", action.payload.items);
+        console.log("Items recibidos:", action.payload.items.juego);
       })
       .addCase(compraCarrito.rejected, (state, action) => {
         state.status = "failed";
